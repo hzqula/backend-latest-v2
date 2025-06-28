5859678c9d8963fb5091d2c78d54a54fb826873b
hzqula <ilooqstrasi@gmail.com>
2025-06-28 18:18:29 +0700

    🎓 add UserRepository with methods for finding, creating, and updating user records

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,16 @@
+import { Prisma, User } from "../../prisma/app/generated/prisma/client";
+import { prisma } from "../configs/prisma";
+
+export class UserRepository {
+  public async findByEmail(email: string): Promise<User | null> {
+    return prisma.user.findUnique({ where: { email } });
+  }
+
+  public async create(data: Prisma.UserCreateInput): Promise<User> {
+    return prisma.user.create({ data });
+  }
+
+  public async update(id: number, data: Prisma.UserUpdateInput): Promise<User> {
+    return prisma.user.update({ where: { id }, data });
+  }
+}
