8ae391b990c6fe23cd555bcd800582664c8dc2c9
hzqula <ilooqstrasi@gmail.com>
2025-06-29 19:13:08 +0700

    🎓 configure CORS middleware with specific options for enhanced security

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,14 @@ import { PORT } from "./configs/env";
 const app: Application = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
